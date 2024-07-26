a2b25903e86f9f8c00177477b0d79c0bb275ab39
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-07-25 19:56:30 -0500

    server 3000 / 2998

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "dotenv/config";
 import "./conectionDB.js";
 import server from "./server.js";
 
-server.listen(2998, () => {
-  console.log("Server Port: 2998");
+server.listen(3000, () => {
+  console.log("Server Port: 3000");
 });
